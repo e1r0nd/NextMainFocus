fa4bc8d458e95d03e060f725e044836a1204f6bd
e1r0nd <e1r0nd.crg@gmail.com>
2016-04-25 00:03:30 +0200

    added bootstrap-material-design

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,42 +1,46 @@
-'use strict';
-
 (function() {
+  'use strict';
 
-class MainController {
+  class MainController {
 
-  constructor($http, $scope, socket) {
-    this.$http = $http;
-    this.socket = socket;
-    this.awesomeThings = [];
+    constructor($http, $scope, socket) {
+      this.$http = $http;
+      this.socket = socket;
+      this.awesomeThings = [];
 
-    $scope.$on('$destroy', function() {
-      socket.unsyncUpdates('thing');
-    });
-  }
+      $scope.$on('$destroy', function() {
+        'use strict';
+        socket.unsyncUpdates('thing');
+      });
+      $.material.init();
+    }
 
-  $onInit() {
-    this.$http.get('/api/things').then(response => {
-      this.awesomeThings = response.data;
-      this.socket.syncUpdates('thing', this.awesomeThings);
-    });
-  }
+    $onInit() {
+      this.$http.get('/api/things')
+        .then(response => {
+          this.awesomeThings = response.data;
+          this.socket.syncUpdates('thing', this.awesomeThings);
+        });
+    }
 
-  addThing() {
-    if (this.newThing) {
-      this.$http.post('/api/things', { name: this.newThing });
-      this.newThing = '';
+    addThing() {
+      if (this.newThing) {
+        this.$http.post('/api/things', {
+          name: this.newThing
+        });
+        this.newThing = '';
+      }
+    }
+
+    deleteThing(thing) {
+      this.$http.delete('/api/things/' + thing._id);
     }
   }
 
-  deleteThing(thing) {
-    this.$http.delete('/api/things/' + thing._id);
-  }
-}
-
-angular.module('nextMainFocusApp')
-  .component('main', {
-    templateUrl: 'app/main/main.html',
-    controller: MainController
-  });
+  angular.module('nextMainFocusApp')
+    .component('main', {
+      templateUrl: 'app/main/main.html',
+      controller: MainController
+    });
 
 })();
