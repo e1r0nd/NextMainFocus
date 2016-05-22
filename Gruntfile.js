d549865e97ea0b509d0f2e7f2f855dcf0b347c3e
e1r0nd <e1r0nd.crg@gmail.com>
2016-05-22 16:44:41 +0200

    clean up

--- Gruntfile.js
+++ Gruntfile.js
@@ -791,10 +791,12 @@ module.exports = function(grunt) {
         ]);
       }
 
-    } else grunt.task.run([
-      'test:server',
-      'test:client'
-    ]);
+    } else {
+      grunt.task.run([
+        'test:server',
+        'test:client'
+      ]);
+    }
   });
 
   grunt.registerTask('build', [
